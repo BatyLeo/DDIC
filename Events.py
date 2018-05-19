49fb5d98d49f87092bf8af72a6bcf727c629c561
BatyLeo <leo.baty@eleves.enpc.fr>
2018-05-19 19:59:07 +0200

    ajout de la structure des évènements

--- Events.py
+++ Events.py
@@ -7,77 +7,40 @@ ressources=['UM','fossile','électricité','nourriture','déchets','pollution']
 jauges=['Economique','Environnemental','Social']
 
 
-
-# création et édition des cartes
-
-def newcard():
-    carte={}
+def newevent():
+    event={}
     Ere=int(input('Ere : '))
     Nom=input('Nom de la carte : ') # string
     Type=input('Type de la carte : ') # string
-    Cout=int(input('Cout en UM de la carte : ')) # int
+    Piste=int(input('Piste : '))
+    Seuils=[]
+    nbseuils=input('nb seuils : ')
+    for k in range(nbseuils):
+        Seuils.append(input('seuil '+str(k)+ ': '))
     Desc=input('Description et effets de la carte : ') # string
-    print('Consommation : ')
-    Cons=[]
-    for i in range(len(ressources)):
-        cons=input('- ' + ressources[i]+' : ')
-        if cons=='':
-            Cons.append(0)
-        else:
-            Cons.append(int(cons))
-    print('Production : ')
-    Prod=[]
-    for i in range(len(ressources)):
-        prod=input('- ' + ressources[i]+' : ')
-        if prod=='':
-            Prod.append(0)
-        else:
-            Prod.append(int(prod))
-    print('Modificateurs : ')
-    Mod=[]
-    for i in range(len(jauges)):
-        mod=input('- ' + jauges[i]+' : ')
-        if mod=='':
-            Mod.append(0)
-        else:
-            Mod.append(int(mod))
-    nbex=input("nombre d'exemplaires : ")
-    if nbex=='':
-        nbex=0
+    Agreg=input('Agregateur : ')
+    Nbex=input("nombre d'exemplaires : ")
+    if Nbex=='':
+        Nbex=0
     else:
-        nbex=int(nbex)
+        Nbex=int(Nbex)
     
-    carte['Nom']=Nom
-    carte['Ere']=Ere
-    carte['Type']=Type
-    carte['Cout']=Cout
-    carte['Description']=Desc
-    carte['Consommation']=Cons
-    carte['Production']=Prod
-    carte['Modificateurs']=Mod
-    carte['Exemplaires']=nbex
-    np.save('Cards/'+Nom,carte)
+    event['Nom']=Nom
+    event['Ere']=Ere
+    event['Type']=Type
+    event['Piste']=Piste
+    event['Seuils']=Seuils
+    event['Agregateurs']=Agreg
+    event['Description']=Desc
+    event['Exemplaires']=Nbex
+    np.save('Events/'+Nom,event)
     
-    DECK1=np.load('DECK1.npy')
-    DECK1=np.append(DECK1,[Nom])
-    np.save('DECK1',DECK1)
-    return carte
+    d=np.load('DeckEvents1.npy')
+    d=np.append(d,[Nom])
+    np.save('DeckEvents1',d)
+    return event
 
-
-def loadDeck(name):
-    deck=np.load(name+'.npy')
-    return deck
-
-
-def actualiseDECK1():
-    deck=loadDeck('DECK1')
-    for card in deck:
-        if(not os.path.isfile('Cards/' + card + '.npy')):
-            deck=np.setdiff1d(deck,card)
-    print(deck)
-    np.save('DECK1',deck)
-
-
+"""    
 def modify_card(nom):
     DECK1=np.load('DECK1.npy')
     DECK1=np.setdiff1d(DECK1,nom)
@@ -124,18 +87,8 @@ def modify_card(nom):
     DECK1=np.append(DECK1,res['Nom'])
     np.save('DECK1',DECK1)
     return res
-        
+"""
 
-def modify_champ(nom,champ,valeur):
-    DECK1=np.load('DECK1.npy')
-    DECK1.remove('Cards/'+nom)
-    res=np.load(nom+'.npy').item()
-    res[champ]=valeur
-    np.save('Cards/'+res['Nom'],res)
-    DECK1=np.append(DECK1,res['Nom'])
-    np.save('DECK1',DECK1)
-    return res
-    
-def load_card(nom):
-    res=np.load('Cards/'+nom+'.npy').item()
+def load_event(nom):
+    res=np.load('Events/'+nom+'.npy').item()
     return res
